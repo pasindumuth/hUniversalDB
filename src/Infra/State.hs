ff834fc2c9fe8630e93404dc428bc747374e4cc3
Pasindu Muthukuda <pasindumuth@gmail.com>
2020-07-02 10:37:29 -0700

    Created tracing for the State infra

--- src/Infra/State.hs
+++ src/Infra/State.hs
@@ -10,31 +10,35 @@ import qualified Debug.Trace as Tr
 import Control.Lens (Lens', Traversal', (.~), (&), (^?!))
 
 import qualified Proto.Actions.Actions as Ac
+import qualified Proto.Messages.TraceMessages as TM
 import Infra.Internal_State
 
-type ST s a = STI ([Ac.OutputAction], Int) s a
+type ST s a = STI ([Ac.OutputAction], Int, [TM.TraceMessage]) s a
 
 runST :: ST s a -> s -> (a, ([Ac.OutputAction], s))
 runST st s =
-  let (ret, ((actions, _), state')) = St.runState st (([], 0), s)
+  let (ret, ((actions, _, _), state')) = St.runState st (([], 0, []), s)
   in (ret, (actions, state'))
 
 debugP :: String -> ST s ()
-debugP message =  St.state $ \((as, cnt), s) ->
-  Tr.trace ("Trace L" ++ show cnt ++ ": " ++ message) ((), ((as, (cnt + 1)), s))
+debugP message =  St.state $ \((as, cnt, trace), s) ->
+  Tr.trace ("Trace L" ++ show cnt ++ ": " ++ message) ((), ((as, (cnt + 1), trace), s))
 
 -- Adds actions. Importantly, this adds actions in reverse.
 addA :: Ac.OutputAction -> ST s ()
-addA a = St.state $ \((as, cnt), s) ->((), ((a:as, cnt), s))
+addA a = St.state $ \((as, cnt, trace), s) ->((), ((a:as, cnt, trace), s))
+
+trace :: TM.TraceMessage -> ST s ()
+trace traceMsg =  St.state $ \((as, cnt, trace), s) ->((), ((as, cnt, traceMsg:trace), s))
 
 -- Dig, update state, and return the return value and actions. 
 runL :: Lens' s1 s2 -> ST s2 a -> ST s1 (a, [Ac.OutputAction])
-runL lens st = St.state $ \((as, cnt), state) ->
-  let (ret, ((as', cnt'), subState)) = St.runState st (([], cnt), state ^?! lens)
-  in ((ret, as'), ((as, cnt'), state & lens .~ subState))
+runL lens st = St.state $ \((as, cnt, trace), state) ->
+  let (ret, ((as', cnt', trace'), subState)) = St.runState st (([], cnt, trace), state ^?! lens)
+  in ((ret, as'), ((as, cnt', trace'), state & lens .~ subState))
 
 -- Dig, update state, and return the return value and actions from a Traversal
 runT :: Traversal' s1 s2 -> ST s2 a -> ST s1 (a, [Ac.OutputAction])
-runT traversal st = St.state $ \((as, cnt), state) ->
-  let (ret, ((as', cnt'), subState)) = St.runState st (([], cnt), state ^?! traversal)
-  in ((ret, as'), ((as, cnt'), state & traversal .~ subState))
+runT traversal st = St.state $ \((as, cnt, trace), state) ->
+  let (ret, ((as', cnt', trace'), subState)) = St.runState st (([], cnt, trace), state ^?! traversal)
+  in ((ret, as'), ((as, cnt', trace'), state & traversal .~ subState))
---
+++ src/Proto/Messages/TraceMessages.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Proto.Messages.TraceMessages where
+
+import qualified Data.Binary as Bn
+import qualified GHC.Generics as Gn
+
+import qualified Proto.Messages.PaxosMessages as PM
+import qualified Proto.Messages.ClientMessages as CM
+
+data TraceMessage =
+  PaxosInsertion String Int PM.PaxosLogEntry |
+  ClientRequestReceived CM.ClientRequest |
+  ClientResponseSent CM.ClientResponse
+  deriving (Gn.Generic, Bn.Binary, Show)
