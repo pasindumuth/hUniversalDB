8201a572714fd6be68801124286824d86a6eb8b2
Pasindu Muthukuda <pasindumuth@gmail.com>
2020-07-02 01:21:00 -0700

    Split the State into two files, one being a very general set of operations

---
+++ src/Infra/Internal_State.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Infra.Internal_State where
+
+import qualified Control.Monad.State as St
+
+import Control.Lens (Lens', Traversal', (.~), (&), (^?!))
+
+infixl 1 .^
+infixl 1 .^^
+infixl 1 .^^.
+infixl 1 .^^^
+
+infixl 1 .^*
+infixl 1 .^^*
+infixl 1 .^^.*
+infixl 1 .^^^*
+
+type STI aux s a = St.State (aux, s) a
+
+makeST :: a -> STI aux s a
+makeST ret = St.state $ \(aux, s) -> (ret, (aux, s))
+
+getL :: Lens' s1 s2 -> STI aux s1 s2
+getL lens = St.state $ \(aux, state) -> (state ^?! lens, (aux, state))
+
+getT :: Traversal' s1 s2 -> STI aux s1 s2
+getT traversal = St.state $ \(aux, state) -> (state ^?! traversal, (aux, state))
+
+-- Dig, update, return, update auxiliary data
+(.^) :: Lens' s1 s2 -> STI aux s2 a -> STI aux s1 a
+(.^) lens st = St.state $ \(aux, state) ->
+  let (ret, (aux', subState)) = St.runState st (aux, state ^?! lens)
+  in (ret, (aux', state & lens .~ subState))
+
+-- Dig, update, return, update auxiliary data from a Traversal.
+(.^*) :: Traversal' s1 s2 -> STI aux s2 a -> STI aux s1 a
+(.^*) traversal st = St.state $ \(aux, state) ->
+  let (ret, (aux', subState)) = St.runState st (aux, state ^?! traversal)
+  in (ret, (aux', state & traversal .~ subState))
+
+-- Dig, update, and return
+(.^^) :: Lens' s1 s2 -> (s2 -> (a, s2)) -> STI aux s1 a
+(.^^) lens func = St.state $ \(aux, state) ->
+  let (ret, subState) = state ^?! lens & func
+  in (ret, (aux, state & lens .~ subState))
+
+-- Dig, update, and return from a Traversal.
+(.^^*) :: Traversal' s1 s2 -> (s2 -> (a, s2)) -> STI aux s1 a
+(.^^*) traversal func = St.state $ \(aux, state) ->
+  let (ret, subState) = state ^?! traversal & func
+  in (ret, (aux, state & traversal .~ subState))
+
+-- Dig and return
+(.^^^) :: Lens' s1 s2 -> (s2 -> a) -> STI aux s1 a
+(.^^^) lens func = St.state $ \(aux, state) ->
+  let ret = state ^?! lens & func
+  in (ret, (aux, state))
+
+-- Dig and return from a Traversal.
+(.^^^*) :: Traversal' s1 s2 -> (s2 -> a) -> STI aux s1 a
+(.^^^*) traversal func = St.state $ \(aux, state) ->
+  let ret = state ^?! traversal & func
+  in (ret, (aux, state))
+
+-- Dig and update
+(.^^.) :: Lens' s1 s2 -> (s2 -> s2) -> STI aux s1 s2
+(.^^.) lens func = St.state $ \(aux, state) ->
+  let subState = state ^?! lens & func
+  in (subState, (aux, state & lens .~ subState))
+
+-- Dig and update from a Traversal.
+(.^^.*) :: Traversal' s1 s2 -> (s2 -> s2) -> STI aux s1 s2
+(.^^.*) traversal func = St.state $ \(aux, state) ->
+  let subState = state ^?! traversal & func
+  in (subState, (aux, state & traversal .~ subState))
--- src/Infra/State.hs
+++ src/Infra/State.hs
@@ -1,103 +1,40 @@
 {-# LANGUAGE RankNTypes #-}
 
-module Infra.State where
+module Infra.State (
+  module Infra.State,
+  module Infra.Internal_State
+) where
 
 import qualified Control.Monad.State as St
 import qualified Debug.Trace as Tr
-
-import qualified Proto.Actions.Actions as Ac
 import Control.Lens (Lens', Traversal', (.~), (&), (^?!))
 
-infixl 1 .^
-infixl 1 .^^
-infixl 1 .^^.
-infixl 1 .^^^
+import qualified Proto.Actions.Actions as Ac
+import Infra.Internal_State
 
-infixl 1 .^*
-infixl 1 .^^*
-infixl 1 .^^.*
-infixl 1 .^^^*
-
-type ST s a = St.State ([Ac.OutputAction], Int, s) a
+type ST s a = STI ([Ac.OutputAction], Int) s a
 
 runST :: ST s a -> s -> (a, ([Ac.OutputAction], s))
 runST st s =
-  let (ret, (actions, _, state')) = St.runState st ([], 0, s)
+  let (ret, ((actions, _), state')) = St.runState st (([], 0), s)
   in (ret, (actions, state'))
 
-makeST :: a -> ST s a
-makeST ret = St.state $ \(as, cnt, s) -> (ret, (as, cnt, s))
-
-trace :: String -> ST s ()
-trace message =  St.state $ \(as, cnt, s) ->
-  Tr.trace ("Trace L" ++ show cnt ++ ": " ++ message) ((), (as, (cnt + 1), s))
+debugP :: String -> ST s ()
+debugP message =  St.state $ \((as, cnt), s) ->
+  Tr.trace ("Trace L" ++ show cnt ++ ": " ++ message) ((), ((as, (cnt + 1)), s))
 
 -- Adds actions. Importantly, this adds actions in reverse.
 addA :: Ac.OutputAction -> ST s ()
-addA a = St.state $ \(as, cnt, s) ->((), (a:as, cnt, s))
-
-getL :: Lens' s1 s2 -> ST s1 s2
-getL lens = St.state $ \(as, cnt, state) -> (state ^?! lens, (as, cnt, state))
-
-getT :: Traversal' s1 s2 -> ST s1 s2
-getT traversal = St.state $ \(as, cnt, state) -> (state ^?! traversal, (as, cnt, state))
+addA a = St.state $ \((as, cnt), s) ->((), ((a:as, cnt), s))
 
 -- Dig, update state, and return the return value and actions. 
 runL :: Lens' s1 s2 -> ST s2 a -> ST s1 (a, [Ac.OutputAction])
-runL lens st = St.state $ \(as, cnt, state) ->
-  let (ret, (as', cnt', subState)) = St.runState st ([], cnt, state ^?! lens)
-  in ((ret, as'), (as, cnt', state & lens .~ subState))
+runL lens st = St.state $ \((as, cnt), state) ->
+  let (ret, ((as', cnt'), subState)) = St.runState st (([], cnt), state ^?! lens)
+  in ((ret, as'), ((as, cnt'), state & lens .~ subState))
 
 -- Dig, update state, and return the return value and actions from a Traversal
 runT :: Traversal' s1 s2 -> ST s2 a -> ST s1 (a, [Ac.OutputAction])
-runT traversal st = St.state $ \(as, cnt, state) ->
-  let (ret, (as', cnt', subState)) = St.runState st ([], cnt, state ^?! traversal)
-  in ((ret, as'), (as, cnt', state & traversal .~ subState))
-
--- Dig, update, return, add Actions
-(.^) :: Lens' s1 s2 -> ST s2 a -> ST s1 a
-(.^) lens st = St.state $ \(as, cnt, state) ->
-  let (ret, (as', cnt', subState)) = St.runState st (as, cnt, state ^?! lens)
-  in (ret, (as', cnt', state & lens .~ subState))
-
--- Dig, update, return, add Actions from a Traversal.
-(.^*) :: Traversal' s1 s2 -> ST s2 a -> ST s1 a
-(.^*) traversal st = St.state $ \(as, cnt, state) ->
-  let (ret, (as', cnt', subState)) = St.runState st (as, cnt, state ^?! traversal)
-  in (ret, (as', cnt', state & traversal .~ subState))
-
--- Dig, update, and return
-(.^^) :: Lens' s1 s2 -> (s2 -> (a, s2)) -> ST s1 a
-(.^^) lens func = St.state $ \(as, cnt, state) ->
-  let (ret, subState) = state ^?! lens & func
-  in (ret, (as, cnt, state & lens .~ subState))
-
--- Dig, update, and return from a Traversal.
-(.^^*) :: Traversal' s1 s2 -> (s2 -> (a, s2)) -> ST s1 a
-(.^^*) traversal func = St.state $ \(as, cnt, state) ->
-  let (ret, subState) = state ^?! traversal & func
-  in (ret, (as, cnt, state & traversal .~ subState))
-
--- Dig and return
-(.^^^) :: Lens' s1 s2 -> (s2 -> a) -> ST s1 a
-(.^^^) lens func = St.state $ \(as, cnt, state) ->
-  let ret = state ^?! lens & func
-  in (ret, (as, cnt, state))
-
--- Dig and return from a Traversal.
-(.^^^*) :: Traversal' s1 s2 -> (s2 -> a) -> ST s1 a
-(.^^^*) traversal func = St.state $ \(as, cnt, state) ->
-  let ret = state ^?! traversal & func
-  in (ret, (as, cnt, state))
-
--- Dig and update
-(.^^.) :: Lens' s1 s2 -> (s2 -> s2) -> ST s1 s2
-(.^^.) lens func = St.state $ \(as, cnt, state) ->
-  let subState = state ^?! lens & func
-  in (subState, (as, cnt, state & lens .~ subState))
-
--- Dig and update from a Traversal.
-(.^^.*) :: Traversal' s1 s2 -> (s2 -> s2) -> ST s1 s2
-(.^^.*) traversal func = St.state $ \(as, cnt, state) ->
-  let subState = state ^?! traversal & func
-  in (subState, (as, cnt, state & traversal .~ subState))
+runT traversal st = St.state $ \((as, cnt), state) ->
+  let (ret, ((as', cnt'), subState)) = St.runState st (([], cnt), state ^?! traversal)
+  in ((ret, as'), ((as, cnt'), state & traversal .~ subState))
